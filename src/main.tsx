e8470a3e8bbc46e81cc8c45277159024a4d7a009
unknown <imane.elharti1@etu.uae.ac.ma>
2026-03-26 21:13:14 +0100

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"; // <-- ajouter
 
-
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/PF"> {/* <-- important pour GitHub Pages */}
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
