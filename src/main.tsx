c4c0c2bdcb403187186ade4e551a3ac2744546b4
unknown <imane.elharti1@etu.uae.ac.ma>
2026-03-26 17:46:16 +0100

    jhgfd

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom"; // <-- ajouter
 
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/PF/"> {/* <-- important pour GitHub Pages */}
+  <BrowserRouter basename="/PF"> {/* <-- important pour GitHub Pages */}
     <App />
   </BrowserRouter>
 );
