3cca675f909084e6b839ca11ac19105b33189ed1
unknown <imane.elharti1@etu.uae.ac.ma>
2026-03-27 13:37:33 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/PF/",
+  base: "/portfolio/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
